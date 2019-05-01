23b459a4449dcd0ead2d689056affae33eac0087
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2019-05-02 00:31:42 +0530

    Adding the basic version of the updated website.

--- index.js
+++ index.js
@@ -41,3 +41,60 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 $(document).ready(function () {
     $(this).scrollTop(0);
 });
+
+// $('#work_details').on('scroll', function(){
+//     if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+//         console.log('Let us move on!');
+//     }
+// });
+
+var my9to5_top = $('#work_main').offset().top;
+var my9to5_bottom = my9to5_top + $('#work_main').height() - $('#my9to5').height();
+
+var writer_top = $('#writer_main').offset().top;
+var writer_bottom = writer_top + $('#writer_main').height() - $('#writer').height();
+
+$(window).scroll(function () {
+    var currentScroll = $(window).scrollTop();
+
+    if (currentScroll >= my9to5_top) {
+        $('#work_main').css({
+            position: 'fixed'
+        });
+    }
+    // if (currentScroll >= my9to5_top && currentScroll < my9to5_bottom) {
+    //     $('#writer').css({
+    //         position: 'static'            
+    //     });
+
+    //     $('#my9to5').css({
+    //         position: 'fixed',
+    //         top: '0',
+    //         left: '0'
+    //     });
+    // } else if (currentScroll >= writer_top && currentScroll < writer_bottom){
+    //     $('#my9to5').css({
+    //         position: 'static'
+    //     });
+
+    //     $('#writer').css({
+    //         position: 'fixed',
+    //         top: '0',
+    //         left: '0'
+    //     });
+    // } else {
+    //     $('#writer').css({
+    //         position: 'static'
+    //     });
+
+    //     $('#my9to5').css({
+    //         position: 'static'
+    //     });
+    // }
+
+    // } else {
+    //     $('#my9to5').css({
+    //         position: 'static'
+    //     });
+    // }
+});
