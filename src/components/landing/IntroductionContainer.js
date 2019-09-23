e6e86acb3d3fbc368a27c9756d84df1ca60c3fd4
Akarsh Agarwal <13bce005@nirmauni.ac.in>
2019-09-23 20:24:51 +0530

    Interchanging the placement of Data and Image

--- src/components/landing/IntroductionContainer.js
+++ src/components/landing/IntroductionContainer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Row, Col } from "antd";
-// import ImageContainer from "./ImageContainer";
+import ImageContainer from "./ImageContainer";
 import LinksContainer from "./LinksContainer";
-import "./IntroductionContainer.css";
 
 const IntroductionContainer = () => {
   return (
     <Row>
-      <Col span={12}>{/* <ImageContainer /> */}</Col>
       <Col span={12}>
-        <LinksContainer />
+        <LinksContainer style={{ zIndex: 100 }} />
+      </Col>
+      <Col span={12}>
+        <ImageContainer style={{ zIndex: -100 }} />
       </Col>
     </Row>
   );
