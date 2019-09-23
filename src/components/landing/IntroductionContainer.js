42a58d6e8bb74000c055e7419ff2e765d466a3ec
Akarsh Agarwal <13bce005@nirmauni.ac.in>
2019-09-23 21:03:47 +0530

    Removing the Image Container

--- src/components/landing/IntroductionContainer.js
+++ src/components/landing/IntroductionContainer.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { Row, Col } from "antd";
-import ImageContainer from "./ImageContainer";
+// import ImageContainer from "./ImageContainer";
 import LinksContainer from "./LinksContainer";
 
 const IntroductionContainer = () => {
   return (
     <Row>
-      <Col span={12}>
-        <LinksContainer style={{ zIndex: 100 }} />
-      </Col>
-      <Col span={12}>
-        <ImageContainer style={{ zIndex: -100 }} />
+      <Col span={24}>
+        <LinksContainer />
       </Col>
     </Row>
   );
