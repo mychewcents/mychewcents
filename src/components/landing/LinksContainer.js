10d95e4a9bc73fa4c6dec7ea8941dad817348282
Akarsh Agarwal <13bce005@nirmauni.ac.in>
2019-09-23 20:01:47 +0530

    Adding the links container

--- src/components/landing/LinksContainer.js
+++ src/components/landing/LinksContainer.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Row, Col } from "antd";
+import "./LinksContainer.css";
+import SocialLinkButton from "./SocialLinksButton";
+import { Links } from "../constants";
 
 const LinksContainer = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="links--default">
+      <Row className="links-content--bottom-align">
+        <Col span={18} offset={3}>
+          <div className="links-name--font">Hey! I&apos;m Akarsh Agarwal</div>
+        </Col>
+        <Col span={18} offset={3} className="links-titles--styling">
+          <div className="links-title--styling">
+            DEVELOPER
+            <div className="links-dot--styling">&middot;</div>
+            WRITER
+            <div className="links-dot--styling">&middot;</div>
+            TRAVELER
+          </div>
+        </Col>
+        <Col offset={3} span={18} className="links-social--styling">
+          {Links.map(link => (
+            <SocialLinkButton
+              key={link.url}
+              typeOfLink={link.type}
+              url={link.url}
+            />
+          ))}
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default LinksContainer;
