fa8e7f4196de61153e30feff20ae2daa2085d4b2
Akarsh Agarwal <13bce005@nirmauni.ac.in>
2019-09-23 21:16:21 +0530

    Moving the files

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-base-setup
-Base setup which can be used for each of the React App that is being built.
---
+++ source/src/components/constants.js
@@ -0,0 +1,26 @@
+export const Links = [
+  {
+    type: "facebook",
+    url: "https://facebook.com/mychewcents"
+  },
+  {
+    type: "twitter",
+    url: "https://twitter.com/mychewcents"
+  },
+  {
+    type: "linkedin",
+    url: "https://linkedin.com/in/mychewcents"
+  },
+  {
+    type: "instagram",
+    url: "https://instagram.com/mychewcents"
+  },
+  {
+    type: "medium",
+    url: "https://medium.com/@mychewcents"
+  },
+  {
+    type: "mail",
+    url: "mailto: akarsh@mychewcents.me"
+  }
+];
