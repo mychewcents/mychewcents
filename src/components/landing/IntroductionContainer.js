7eb111a00063b78000f464de3997e5b90bd64695
Akarsh Agarwal <13bce005@nirmauni.ac.in>
2019-09-23 20:01:25 +0530

    Using the Links Container

--- src/components/landing/IntroductionContainer.js
+++ src/components/landing/IntroductionContainer.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { Row, Col } from "antd";
-import ImageContainer from "./ImageContainer";
+// import ImageContainer from "./ImageContainer";
 import LinksContainer from "./LinksContainer";
+import "./IntroductionContainer.css";
 
 const IntroductionContainer = () => {
   return (
-    <div>
-      <Row>
-        <Col span={10}>
-          Hello
-          {/* <LinksContainer /> */}
-        </Col>
-        <Col span={10}>
-          Hello
-          {/* <LinksContainer /> */}
-        </Col>
-      </Row>
-    </div>
+    <Row>
+      <Col span={12}>{/* <ImageContainer /> */}</Col>
+      <Col span={12}>
+        <LinksContainer />
+      </Col>
+    </Row>
   );
 };
 
