ce89d925ff84b4a2805f39eebe98988202e3a965
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-05-03 21:18:23 +0530

    Correcting the footer layout.

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -8,26 +8,36 @@ const Footer = () => {
   return (
     <div>
       <Row className='footer-home'>
-        <Col
-          className='footer-components'
-          xl={{ span: 18, offset: 3 }}
-          xs={{ span: 22, offset: 1 }}
-        >
-          <div className='footer-title'>
-            Reach me: <span className='username'>@mychewcents</span>
-          </div>
-          <div className='footer-links'>
-            {LINKS.map((link: any, index: number) => (
-              <Button key={index} type='link' href={link.url} target='_blank'>
-                <Icon
-                  type={link.type}
-                  className='icon-style'
-                  style={{ fontSize: 30, color: 'white' }}
-                  // theme='outlined'
-                />
-              </Button>
-            ))}
-          </div>
+        <Col xl={{ span: 18, offset: 3 }} xs={{ span: 22, offset: 1 }}>
+          <Row className='footer-container'>
+            <Col
+              lg={{ span: 16 }}
+              xs={{ span: 24 }}
+              className='footer-components'
+            >
+              <div className='footer-title'>
+                Reach me <span className='username'>@mychewcents</span>
+              </div>
+            </Col>
+            <Col lg={{ span: 8 }} xs={{ span: 24 }}>
+              <div className='footer-links'>
+                {LINKS.map((link: any, index: number) => (
+                  <Button
+                    key={index}
+                    type='link'
+                    href={link.url}
+                    target='_blank'
+                  >
+                    <Icon
+                      type={link.type}
+                      className='icon-style'
+                      style={{ fontSize: 30, color: 'white' }}
+                    />
+                  </Button>
+                ))}
+              </div>
+            </Col>
+          </Row>
         </Col>
       </Row>
       <Row>
